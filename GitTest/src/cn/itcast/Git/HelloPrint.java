5b995f166e737b5aa24cf1a0964dbf25fb9fad1c
lijiayao <lijiayao@itcast.cn>
2020-06-16 21:04:56 +0800

    version2

--- GitTest/src/cn/itcast/Git/HelloPrint.java
+++ GitTest/src/cn/itcast/Git/HelloPrint.java
@@ -3,5 +3,6 @@ package cn.itcast.Git;
 public class HelloPrint {
     public static void main(String[] args) {
         System.out.println("this is version1");
+        System.out.println("this is version2");
     }
 }
